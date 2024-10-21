81d59383211f14ec6d9152d3daba40dfa975d4f0
Kai <kkai.chien0813@gmail.com>
2024-10-21 10:21:05 -0700

    style: style the tabs

--- client/src/components/ui/tabs.tsx
+++ client/src/components/ui/tabs.tsx
@@ -3,22 +3,22 @@ import "react-tabs/style/react-tabs.css";
 
 function TabsComponent() {
 	return (
-		<Tabs>
-			<TabList className="flex border-b gap-3">
+		<Tabs className="absolute top-12 left-0 w-full mx-10">
+			<TabList className="flex border-b border-custom-border  gap-3">
 				<Tab
-					className="px-4 py-2 cursor-pointer bg-white/50 focus:outline-none hover:opacity-70 border border-custom-border rounded-t-md"
+					className="px-4 py-2 cursor-pointer bg-white/50 focus:outline-none hover:opacity-70 border-t border-x border-custom-border rounded-t-md"
 					selectedClassName="!bg-transparent"
 				>
 					Tab 1
 				</Tab>
 				<Tab
-					className="px-4 py-2 cursor-pointer bg-white/50 focus:outline-none hover:opacity-70 border border-custom-border rounded-t-md"
+					className="px-4 py-2 cursor-pointer bg-white/50 focus:outline-none hover:opacity-70 border-t border-x border-custom-border rounded-t-md"
 					selectedClassName="!bg-transparent"
 				>
 					Tab 2
 				</Tab>
 				<Tab
-					className="px-4 py-2 cursor-pointer bg-white/50 focus:outline-none hover:opacity-70 border border-custom-border rounded-t-md"
+					className="px-4 py-2 cursor-pointer bg-white/50 focus:outline-none hover:opacity-70 border-t border-x border-custom-border rounded-t-md"
 					selectedClassName="!bg-transparent"
 				>
 					Tab 3
