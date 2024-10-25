9ceaffe13a8856e98df312e873ad9ed1e1a589e5
Kai <kkai.chien0813@gmail.com>
2024-10-25 11:05:11 -0700

    chore: display mock task data with card

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,5 +1,30 @@
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import tasksData from "@/mock/tasksData.json";
+import { Badge } from "./ui/badge";
+
 function Board() {
-	return <p>Board Page</p>;
+	return (
+		<div className="grid gap-3">
+			{tasksData.map((task) => (
+				<Card key={task.id}>
+					<CardHeader>
+						<CardTitle className="text-custom-text">{task.title}</CardTitle>
+						<Badge>badge</Badge>
+						<CardDescription>{task.due_date}</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<p>{task.description}</p>
+					</CardContent>
+				</Card>
+			))}
+		</div>
+	);
 }
 
 export default Board;
