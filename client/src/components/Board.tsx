c1b54c2dece31ff09acf18d09fb7b32fa526cc46
Kai <kkai.chien0813@gmail.com>
2024-10-27 20:49:11 -0700

    feat: display data on board

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -6,22 +6,45 @@ import {
 	CardTitle,
 } from "@/components/ui/card";
 import tasksData from "@/mock/tasksData.json";
+import groupData from "@/mock/groupsData.json";
+import type { ITask } from "@/types/data";
 import { Badge } from "./ui/badge";
 
 function Board() {
+	const tasks: ITask[] = tasksData as ITask[];
+	const groupedTasks = tasks.reduce((acc: Record<string, ITask[]>, task) => {
+		if (!acc[task.status]) {
+			acc[task.status] = [];
+		}
+
+		acc[task.status].push(task);
+		return acc;
+	}, {});
+
+	const groups = groupData.reduce((acc: Record<string, string>, group) => {
+		acc[group.id] = group.name;
+		return acc;
+	}, {});
+
 	return (
-		<div className="grid gap-3">
-			{tasksData.map((task) => (
-				<Card key={task.id}>
-					<CardHeader>
-						<CardTitle className="text-custom-text">{task.title}</CardTitle>
-						<Badge>badge</Badge>
-						<CardDescription>{task.due_date}</CardDescription>
-					</CardHeader>
-					<CardContent>
-						<p>{task.description}</p>
-					</CardContent>
-				</Card>
+		<div className="flex justify-evenly gap-4 p-4">
+			{Object.keys(groupedTasks).map((status) => (
+				<div key={status}>
+					<Badge className="capitalize mb-3">{status.replace("_", " ")}</Badge>
+
+					{groupedTasks[status].map((task) => (
+						<Card key={task.id} className="mb-5">
+							<CardHeader>
+								<CardTitle className="text-custom-text">{task.title}</CardTitle>
+								<Badge>{groups[task.group_id]}</Badge>
+								<CardDescription>{task.due_date}</CardDescription>
+							</CardHeader>
+							<CardContent>
+								<p>{task.description}</p>
+							</CardContent>
+						</Card>
+					))}
+				</div>
 			))}
 		</div>
 	);
