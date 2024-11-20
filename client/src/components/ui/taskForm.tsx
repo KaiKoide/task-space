686db35e8392bdcc41569abfe64d9bd1d3244ce3
Kai <kkai.chien0813@gmail.com>
2024-11-19 23:29:59 -0800

    feat: add textare to form

--- client/src/components/ui/taskForm.tsx
+++ client/src/components/ui/taskForm.tsx
@@ -13,20 +13,53 @@ import {
 	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+} from "@/components/ui/command";
+import { Check, ChevronsUpDown } from "lucide-react";
 
 const formSchema = z.object({
-	username: z.string().min(2, {
+	title: z.string().min(2, {
 		message: "Username must be at least 2 characters.",
 	}),
 	email: z.string(),
+	desc: z.string(),
+	language: z.string({
+		required_error: "Please select a language.",
+	}),
 });
+
+const languages = [
+	{ label: "English", value: "en" },
+	{ label: "French", value: "fr" },
+	{ label: "German", value: "de" },
+	{ label: "Spanish", value: "es" },
+	{ label: "Portuguese", value: "pt" },
+	{ label: "Russian", value: "ru" },
+	{ label: "Japanese", value: "ja" },
+	{ label: "Korean", value: "ko" },
+	{ label: "Chinese", value: "zh" },
+] as const;
 
 function TaskForm() {
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
-			username: "",
+			title: "",
 			email: "",
+			desc: "",
 		},
 	});
 
@@ -39,15 +72,76 @@ function TaskForm() {
 			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
 				<FormField
 					control={form.control}
-					name="username"
+					name="title"
 					render={({ field }) => (
 						<FormItem>
-							<FormLabel>Username</FormLabel>
+							<FormLabel>Task title</FormLabel>
 							<FormControl>
-								<Input placeholder="shadcn" {...field} />
+								<Input placeholder="title" {...field} />
 							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="language"
+					render={({ field }) => (
+						<FormItem className="flex flex-col">
+							<FormLabel>Language</FormLabel>
+							<Popover>
+								<PopoverTrigger asChild>
+									<FormControl>
+										<Button
+											variant="outline"
+											// biome-ignore lint/a11y/useSemanticElements: <explanation>
+											role="combobox"
+											className={cn(
+												"w-[200px] justify-between",
+												!field.value && "text-muted-foreground",
+											)}
+										>
+											{field.value
+												? languages.find(
+														(language) => language.value === field.value,
+													)?.label
+												: "Select language"}
+											<ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+										</Button>
+									</FormControl>
+								</PopoverTrigger>
+								<PopoverContent className="w-[200px] p-0 pointer-events-auto">
+									<Command>
+										<CommandInput placeholder="Search language..." />
+										<CommandList>
+											<CommandEmpty>No language found.</CommandEmpty>
+											<CommandGroup>
+												{languages.map((language) => (
+													<CommandItem
+														value={language.label}
+														key={language.value}
+														onSelect={() => {
+															form.setValue("language", language.value);
+														}}
+													>
+														{language.label}
+														<Check
+															className={cn(
+																"ml-auto",
+																language.value === field.value
+																	? "opacity-100"
+																	: "opacity-0",
+															)}
+														/>
+													</CommandItem>
+												))}
+											</CommandGroup>
+										</CommandList>
+									</Command>
+								</PopoverContent>
+							</Popover>
 							<FormDescription>
-								This is your public display name.
+								This is the language that will be used in the dashboard.
 							</FormDescription>
 							<FormMessage />
 						</FormItem>
@@ -65,6 +159,19 @@ function TaskForm() {
 							<FormDescription>
 								This is your public display name.
 							</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="desc"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Description</FormLabel>
+							<FormControl>
+								<Textarea placeholder="Description" {...field} />
+							</FormControl>
 							<FormMessage />
 						</FormItem>
 					)}
