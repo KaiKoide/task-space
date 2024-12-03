db0b98dc89f320008190498f0516f8c1989b0e18
Kai <kkai.chien0813@gmail.com>
2024-12-02 16:19:09 -0800

    chore: build a GraphQL server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,28 @@
-import express, { type Request, type Response } from "express";
-import { graphqlHTTP } from "express-graphql";
-import { GraphQLSchema, buildSchema } from "graphql";
+import express, {
+	type Request,
+	type Response,
+	type Application,
+} from "express";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./schema";
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+(async () => {
+	await server.start();
+	server.applyMiddleware({ app } as any);
+
+	app.listen(PORT, () => {
+		console.log(`Server is running on http://localhost:${PORT}`);
+		console.log(
+			`GraphQL Playground available at http://localhost:${PORT}${server.graphqlPath}`,
+		);
+	});
+})();
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello, TypeScript with Express!");
 });
-
-app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
-});
-
-// let sampleUsers = [
-
-// ]
