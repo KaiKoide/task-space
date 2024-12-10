8d03a9d85bea3dac282de9e697ae89e1694edc5e
Kai <kkai.chien0813@gmail.com>
2024-12-09 20:09:47 -0800

    feat: fetch data from supabase

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,23 @@
-import express, {
-	type Request,
-	type Response,
-	type Application,
-} from "express";
-import { type GraphQLSchema, buildSchema } from "graphql";
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs, resolvers } from "./schema.js";
+import express, { type Request, type Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
-const server = new ApolloServer({
-	typeDefs,
-	resolvers,
+const app = express();
+const port = 3000;
+const prisma = new PrismaClient();
+
+// app.get("/", (req: Request, res: Response) => {
+// 	res.send("Hello World!");
+// });
+
+app.get("/", async (req: Request, res: Response) => {
+	try {
+		const groups = await prisma.group.findMany();
+		res.status(200).json(groups);
+		res.send(groups);
+	} catch (error) {
+		console.error("Error fetching groups:", error);
+		res.status(500).json({ error: "Failed to fetch groups" });
+	}
 });
 
-const { url } = await startStandaloneServer(server, {
-	listen: { port: 3000 },
-});
-
-console.log(`🚀  Server ready at: ${url}`);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
