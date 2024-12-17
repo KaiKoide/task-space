842d296082e8ce1f3904bc454eee9bff33043d3b
Kai <kkai.chien0813@gmail.com>
2024-12-17 11:36:51 -0800

    chore: create api folder

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,10 +1,10 @@
 import cors from "cors";
 import express, { type Request, type Response } from "express";
 
-import { groupsRoute } from "./routes/group.route.js";
-import { statusRoute } from "./routes/status.route.js";
-import { userRoute } from "./routes/user.route.js";
-import { taskRoute } from "./routes/task.route.js";
+import { groupsRoute } from "../src/routes/group.route.js";
+import { statusRoute } from "../src/routes/status.route.js";
+import { userRoute } from "../src/routes/user.route.js";
+import { taskRoute } from "../src/routes/task.route.js";
 
 const app = express();
 const port = 3000;
