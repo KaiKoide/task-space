7c5340e610b68cbca7315908a414021c3395dc18
Kai <kkai.chien0813@gmail.com>
2024-12-18 13:42:52 -0800

    feat: add sign up feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,32 @@
-import "./App.css";
+import {
+	SignedIn,
+	SignedOut,
+	UserButton,
+	RedirectToSignUp,
+	useUser,
+} from "@clerk/clerk-react";
+
 import TabsComponent from "./components/ui/tabs";
 
 function App() {
+	const { isSignedIn, user, isLoaded } = useUser();
+
+	console.log(isSignedIn, user, isLoaded);
+
 	return (
 		<>
-			<TabsComponent />
+			{/* When you're not logged in */}
+			<SignedOut>
+				<RedirectToSignUp />
+			</SignedOut>
+
+			{/* When you're logged in */}
+			<SignedIn>
+				<div style={{ padding: "20px" }}>
+					<UserButton />
+					<TabsComponent />
+				</div>
+			</SignedIn>
 		</>
 	);
 }
